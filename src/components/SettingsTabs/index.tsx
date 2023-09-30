0ad2d184f700482952f11efa9cf1b34c3df83ae9
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-09-30 16:42:31 -0300

    feat: tab animation

--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -39,10 +39,19 @@ export function SettingsTabs() {
         />
         <TabItem
           value="tab8"
-          title="Integrations"
+          title="Notification"
           isSelected={currentTab === 'tab8'}
         />
-        <TabItem value="tab9" title="API" isSelected={currentTab === 'tab9'} />
+        <TabItem
+          value="tab9"
+          title="Integrations"
+          isSelected={currentTab === 'tab9'}
+        />
+        <TabItem
+          value="tab10"
+          title="API"
+          isSelected={currentTab === 'tab10'}
+        />
       </Tabs.List>
     </Tabs.Root>
   )
