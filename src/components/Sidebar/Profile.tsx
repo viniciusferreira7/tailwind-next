f04af205aac4e170cce6a8c440fbc5f404b286a7
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-09-30 18:26:03 -0300

    fix: User Profile

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -1,8 +1,9 @@
+import { LogOut } from 'lucide-react'
 import Image from 'next/image'
 
 export function Profile() {
   return (
-    <div className="flex items-center gap-3">
+    <div className="grid grid-cols-profile items-center gap-3">
       <Image
         width={40}
         height={40}
@@ -10,6 +11,17 @@ export function Profile() {
         className="h-10 w-10 rounded-full"
         alt=""
       />
+      <div className="flex flex-col truncate">
+        <span className="text-sm font-semibold text-zinc-700">
+          Vinicius Ferreira
+        </span>
+        <span className="truncate text-sm text-zinc-500">
+          viniciusaquinodev07@gmail.com
+        </span>
+      </div>
+      <button type="button" className="ml-auto rounded-md p-2 hover:bg-zinc-50">
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
     </div>
   )
 }
