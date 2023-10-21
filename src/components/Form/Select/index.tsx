d3ece21155542dd5d8d963fb2321be8ad6b08dc7
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-10-21 17:30:46 -0300

    feat: Biography input

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -4,14 +4,14 @@ import * as SelectPrimitive from '@radix-ui/react-select'
 import { ChevronDown } from 'lucide-react'
 import { ReactNode } from 'react'
 
-interface SelectProps {
+interface SelectProps extends SelectPrimitive.SelectProps {
   children: ReactNode
   placeholder: string
 }
 
-export function Select({ children, placeholder }: SelectProps) {
+export function Select({ children, placeholder, ...props }: SelectProps) {
   return (
-    <SelectPrimitive.Root>
+    <SelectPrimitive.Root {...props}>
       <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
         <SelectPrimitive.Value
           placeholder={placeholder}
@@ -26,7 +26,7 @@ export function Select({ children, placeholder }: SelectProps) {
           side="bottom"
           sideOffset={8}
           position="popper"
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white"
+          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
