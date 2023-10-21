41662b39a179f68e9a14640fa36c41fd52f8a09f
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-10-21 16:45:38 -0300

    feat: Componentize Select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import * as FileInput from '@/components/Form/FileInput'
 import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 import * as Input from '@/components/Input'
 import { SettingsTabs } from '@/components/SettingsTabs'
 import { Mail } from 'lucide-react'
@@ -103,7 +104,13 @@ export default function Home() {
             >
               Country
             </label>
-            <Select />
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
           </div>
           <div className="grid grid-cols-form gap-3 pt-5">
             <label
@@ -112,7 +119,10 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div />
+            <Select placeholder="Select a country">
+              <SelectItem value="br" text="Brasil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
           <div className="grid grid-cols-form gap-3 pt-5">
             <label htmlFor="bio" className="text-sm font-medium text-zinc-700">
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -21,7 +21,7 @@ type RootProps = ComponentProps<'div'>
 export function Root(props: RootProps) {
   return (
     <div
-      className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
       {...props}
     />
   )
