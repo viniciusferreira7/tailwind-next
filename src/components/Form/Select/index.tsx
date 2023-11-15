dbe596a701e1a0ebf00a83b6a8f35e80ad9a9215
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-11-15 19:15:03 -0300

    feat: Animating the opening of the Selects

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -26,7 +26,7 @@ export function Select({ children, placeholder, ...props }: SelectProps) {
           side="bottom"
           sideOffset={8}
           position="popper"
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
