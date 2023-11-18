534439196ba69b02c9a51ea48fd4c199960d47d1
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-11-18 11:51:36 -0300

    feat: Creating focus effects

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -12,7 +12,7 @@ interface SelectProps extends SelectPrimitive.SelectProps {
 export function Select({ children, placeholder, ...props }: SelectProps) {
   return (
     <SelectPrimitive.Root {...props}>
-      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-600">
+      <SelectPrimitive.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100 data-[placeholder]:text-zinc-600">
         <SelectPrimitive.Value
           placeholder={placeholder}
           className="text-black"
--- src/components/Form/Textarea.tsx
+++ src/components/Form/Textarea.tsx
@@ -5,7 +5,7 @@ type TextareaProps = ComponentProps<'textarea'>
 export function Textarea({ ...props }: TextareaProps) {
   return (
     <textarea
-      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
       {...props}
     />
   )
