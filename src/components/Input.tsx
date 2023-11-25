28bd6fd1f40f18779d1fa0a3122222b4bdcbff07
Vinicius Ferreira <viniciuferreira32@gmail.com>
2023-11-25 15:22:55 -0300

    feat: Dark sidebar theme

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
 type PrefixProps = ComponentProps<'div'>
 
@@ -10,7 +11,7 @@ type ControlProps = ComponentProps<'input'>
 export function Control(props: ControlProps) {
   return (
     <input
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
+      className="dark:text-zinc-10 flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none dark:text-zinc-100 dark:placeholder-zinc-400"
       {...props}
     />
   )
@@ -21,7 +22,12 @@ type RootProps = ComponentProps<'div'>
 export function Root(props: RootProps) {
   return (
     <div
-      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100"
+      className={twMerge(
+        'flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm',
+        'focus-within:border-violet-300 focus-within:ring-4 focus-within:ring-violet-100',
+        'dark:border-zinc-700 dark:bg-zinc-800 dark:focus-within:border-violet-500 dark:focus-within:ring-violet-500/20',
+        props.className,
+      )}
       {...props}
     />
   )
--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -13,10 +13,10 @@ export function Profile() {
         alt=""
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Vinicius Ferreira
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="truncate text-sm text-zinc-500 dark:text-zinc-400">
           viniciusaquinodev07@gmail.com
         </span>
       </div>
